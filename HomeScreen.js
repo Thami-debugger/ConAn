aa1031d2dbbf9d1c4354a7f3c4039217ccb9f5b3
Thami Nkosivelaphi <nkosivelaphi.t@gmail.com>
2026-06-06 00:40:51 +0200

    Fix phone connection errors with same-origin API fallback and Vercel rewrites

--- HomeScreen.js
+++ HomeScreen.js
@@ -10,7 +10,7 @@ import {
 
 const FALLBACK_API_BASE_URL = Platform.select({
   android: "http://10.0.2.2:8000",
-  default: "http://127.0.0.1:8000",
+  default: typeof window !== "undefined" ? window.location.origin : "http://127.0.0.1:8000",
 });
 const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || FALLBACK_API_BASE_URL;
 const VOICE_THRESHOLD = 0.008;
